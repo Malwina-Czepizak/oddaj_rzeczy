78004e7933923253edc42b9695c3c81a0515fca8
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-11-02 18:52:02 +0100

    Finished HomeStatistics section

--- src/layouts/Home/HomeStatistics/index.js
+++ src/layouts/Home/HomeStatistics/index.js
@@ -3,9 +3,23 @@ import "./HomeStatistics.scss";
 
 function HomeStatistics() {
     return (
-        <section>
-            <h2>Statistics (3 columns) section</h2>
-        </section>
+        <summary className="home__statistics">
+            <div className="home__statistics__container">
+                <h2>10</h2>
+                <h3>ODDANYCH WORKÓW</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div className="home__statistics__container">
+                <h2>5</h2>
+                <h3>WSPARTYCH ORGANIZACJI</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+            <div className="home__statistics__container">
+                <h2>7</h2>
+                <h3>ZORGANIZOWANY ZBIÓREK</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+            </div>
+        </summary>
     );
 }
 
