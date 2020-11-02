2a4a614a104fd35f4bc47cacfb61f7712a7f0652
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-11-02 20:16:37 +0100

    Added router function to btn

--- src/components/DonateBtn/index.js
+++ src/components/DonateBtn/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import "./DonateBtn.scss";
+import {Link} from "react-router-dom";
 
 export function GiveThingsBtn() {
-    return <button>Oddaj rzeczy</button>
+    return <button><Link to="/logowanie">Oddaj rzeczy</Link></button>
 }
 
 export function ArrangeDonationBtn() {
