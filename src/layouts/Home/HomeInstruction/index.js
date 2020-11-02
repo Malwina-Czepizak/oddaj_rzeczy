a7a8dd7ccedaf9d0b34009f4189573937ccb1f0c
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-11-02 20:17:12 +0100

    Finished HomeInstruction section

--- src/layouts/Home/HomeInstruction/index.js
+++ src/layouts/Home/HomeInstruction/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import "./HomeInstruction.scss";
+import decorator from "../../../assets/Decoration.svg";
+import shirt from "../../../assets/Icon-1.svg";
+import bag from "../../../assets/Icon-2.svg";
+import loupe from "../../../assets/Icon-3.svg";
+import repeat from "../../../assets/Icon-4.svg";
+import {GiveThingsBtn} from "../../../components/DonateBtn";
+
 
 function HomeInstruction() {
     return (
-        <section name="instruction">
-            <h2>4 steps section</h2>
+        <section name="instruction" className="home__instruction">
+            <div className="home__instruction__header">
+                <h2>Wystarczą 4 proste kroki</h2>
+                <img src={decorator} alt="decoration"/>
+            </div>
+            <div className="home__instruction__main">
+                <div className="home__instruction__main__content">
+                    <img src={shirt} alt="t-shirt"/>
+                    <span>Wybierz rzeczy</span>
+                    <hr className="line"/>
+                    <span>ubrania, zabawki, sprzęt i inne</span>
+                </div>
+                <div className="home__instruction__main__content">
+                    <img src={bag} alt="bag"/>
+                    <span>Spakuj je</span>
+                    <hr className="line"/>
+                    <span>skorzystaj z worków na śmieci</span>
+                </div>
+                <div className="home__instruction__main__content">
+                    <img src={loupe} alt="magnifying glass"/>
+                    <span>Zdecyduj komu chcesz pomóc</span>
+                    <hr className="line"/>
+                    <span>wybierz zaufane miejsce</span>
+                </div>
+                <div className="home__instruction__main__content">
+                    <img src={repeat} alt="repeat circle"/>
+                    <span>Zamów kuriera</span>
+                    <hr className="line"/>
+                    <span>kurier przyjedzie w dogodnym terminie</span>
+                </div>
+            </div>
+            <GiveThingsBtn />
         </section>
     );
 }
