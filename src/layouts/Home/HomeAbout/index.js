f499483cee5ef8a774bba3ba763600b39d1cb703
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-11-03 18:14:14 +0100

    Finished About section

--- src/layouts/Home/HomeAbout/index.js
+++ src/layouts/Home/HomeAbout/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import "./HomeAbout.scss";
+import decorator from "../../../assets/Decoration.svg";
+import signature from "../../../assets/Signature.svg"
 
 function HomeAbout() {
     return (
-        <section name="about">
-            <h2>About section</h2>
+        <section name="about" className="home__about">
+            <div className="home__about__text">
+                <h2>O nas</h2>
+                <img src={decorator} alt="decoration"/>
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <img src={signature} alt="signature" className="signature"/>
+            </div>
+            <div className="home__about__image"/>
         </section>
     );
 }
