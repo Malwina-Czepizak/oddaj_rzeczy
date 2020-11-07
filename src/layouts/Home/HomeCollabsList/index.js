fe0d5d8a9b2efdd6dbd765b06fa7ec3adfc5dbf5
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-11-07 09:17:22 +0100

    Prepered HomeCollabList component

--- src/layouts/Home/HomeCollabsList/index.js
+++ src/layouts/Home/HomeCollabsList/index.js
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./HomeCollabsList.scss";
+import decorator from "../../../assets/Decoration.svg";
 
 function HomeCollabsList() {
+    const [data, setData] = useState(false);
+    const [dataName, setDataName] = useState(0);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/collaboration`)
+            .then(response => response.json())
+            .then(response => {
+                setData(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []);
+
+    const handleClick = (index) => {
+        setDataName(index);
+    }
+
     return (
-        <section name="collabs">
-            <h2>Collabs section</h2>
+        <section name="collabs" className="home__collabs">
+            <div className="home__collabs__text__container">
+                <h2>Komu pomagamy?</h2>
+                <img src={decorator} alt="decoration"/>
+                <div className="home__collabs__btn__container">
+                    <button onClick={()=>handleClick(0)}>Fundacjom</button>
+                    <button onClick={()=>handleClick(1)}>Organizacjom pozarządowym</button>
+                    <button onClick={()=>handleClick(2)}>Lokalnym zbiórkom</button>
+                </div>
+                {data &&
+                <p>{data[dataName].desc}</p>
+                }
+            </div>
+            <div className="home__collabs__organizations">
+                 <ul>
+                     {data &&
+                     data[dataName].items.map((data, i) =>
+                        <li key={i}>
+                            <span>{data.header}</span>
+                            <span>{data.subheader}</span>
+                            <span>{data.desc}</span>
+                        </li>)
+                     }
+                 </ul>
+            </div>
         </section>
     );
 }
