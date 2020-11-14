a993fcb21c0dc6b66d66933e133b372e3a693334
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-11-14 13:58:56 +0100

    Register section finished

--- src/layouts/Register/index.js
+++ src/layouts/Register/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import NavigationWithHashLink from "../../components/NavigationWithHashLink";
+import decorator from "../../assets/Decoration.svg";
+import {Link} from "react-router-dom";
+import "./Register.scss";
 
 function Register() {
     return (
-        <div>
-            <h1>Rejestracja</h1>
+        <div className="register">
+            <NavigationWithHashLink />
+            <div className="form__container">
+                <h1>Załóż konto</h1>
+                <img src={decorator} alt="decoration"/>
+                <form>
+                    <div>
+                        <label>Email</label>
+                        <input/>
+                    </div>
+                    <div>
+                        <label>Hasło</label>
+                        <input/>
+                    </div>
+                    <div>
+                        <label>Powtórz hasło</label>
+                        <input/>
+                    </div>
+                </form>
+                <div className="form__buttons">
+                    <button><Link to="/logowanie">Zaloguj się</Link></button>
+                    <button>Załóż konto</button>
+                </div>
+            </div>
         </div>
     );
 }
