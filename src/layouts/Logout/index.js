37773df9ad1e0d59ac79b863a157b52e50bd4470
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-11-14 15:01:54 +0100

    Logout section finished

--- src/layouts/Logout/index.js
+++ src/layouts/Logout/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import NavigationWithHashLink from "../../components/NavigationWithHashLink";
+import decorator from "../../assets/Decoration.svg";
+import {Link} from "react-router-dom";
+import "./Logout.scss";
 
 function Logout() {
     return (
-        <div>
-            <h1>Wylogowano</h1>
+        <div className="logout">
+            <NavigationWithHashLink />
+            <div className="form__container">
+                <h1>Wylogowanie nastąpiło pomyślnie!</h1>
+                <img src={decorator} alt="decoration"/>
+                <div className="form__buttons">
+                    <button><Link to="/">Strona główna</Link></button>
+                </div>
+            </div>
         </div>
     );
 }
