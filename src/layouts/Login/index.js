959bfd02880f7be7a4dde55a59253730f00ea1e4
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-11-14 13:45:18 +0100

    Login section finished

--- src/layouts/Login/index.js
+++ src/layouts/Login/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import "./Login.scss";
 import NavigationWithHashLink from "../../components/NavigationWithHashLink";
+import decorator from "../../assets/Decoration.svg"
 
 function Login() {
     return (
         <div className="login">
             <NavigationWithHashLink />
+            <div className="form__container">
+                <h1>Zalog się</h1>
+                <img src={decorator} alt="decoration"/>
+                <form>
+                    <div>
+                        <label>Email</label>
+                        <input/>
+                    </div>
+                    <div>
+                        <label>Hasło</label>
+                        <input/>
+                    </div>
+                </form>
+                <div className="form__buttons">
+                    <button><Link to="/rejestracja">Załóż konto</Link></button>
+                    <button>Zaloguj się</button>
+                </div>
+            </div>
         </div>
     );
 }
